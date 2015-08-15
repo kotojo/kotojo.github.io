2663db8898044e504cca81ca126744cb8ba850bb
Bruce <brucek17@hotmail.com>
2015-08-15 15:49:18 -0400

    made almost the whole portfolio, but have some css and styling stuff to do... maybe more

---
+++ main.js
@@ -0,0 +1,26 @@
+$(function() {
+        $(window).bind('scroll', function(){
+          var navHeight = $(window).height() - 70;
+
+          if ( $(window).scrollTop() > navHeight ) {
+            $('nav').addClass('fixed');
+          } else {
+            $('nav').removeClass('fixed');
+          }
+
+        });
+
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target == target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+
+});
